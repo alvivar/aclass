3364b73cb787a7bc2cc1e79343bc68a9467ec7b4
alvivar <andresalvivar@gmail.com>
2017-11-09 08:46:46 -0600

    argsparse ready for functionality

--- aclass.py
+++ aclass.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 import re
@@ -36,24 +37,43 @@ if __name__ == "__main__":
             sys.executable if getattr(sys, 'frozen', False) else __file__))
     os.chdir(DIR)
 
+    # Command line args
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument(
+        "-u", "--urls", help="urls to analyze", nargs="+", default=[])
+    PARSER.add_argument(
+        "-f",
+        "--files",
+        help="files with urls to analyze",
+        nargs="+",
+        default=[])
+    PARSER.add_argument(
+        "-e",
+        "--export",
+        help="export html bookmarks file",
+        nargs="?",
+        type=str,
+        const="bookmark.html")
+    ARGS = PARSER.parse_args()
+
     HEADER = {
         'User-Agent':
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
     }
 
-    STOP_ES = json.load(open("stop-es.json", "r"))
-    STOP_EN = json.load(open("stop-en.json", "r"))
-    STOP_WORDS = STOP_EN + STOP_ES
-
     WORDS = extract_words(
         "https://stackoverflow.com/questions/8113782/split-string-on-whitespace-in-python",
         headers=HEADER)
+
+    STOP_ES = json.load(open("stop-es.json", "r"))
+    STOP_EN = json.load(open("stop-en.json", "r"))
+    STOP_WORDS = STOP_EN + STOP_ES
     CLEAN = [w for w in WORDS if w and w.lower() not in STOP_WORDS]
 
     with open("debug.txt", "w", encoding="utf-8") as f:
         f.write("\n".join(CLEAN))
 
-    COUNT = Counter(CLEAN)
+    COUNT = Counter(CLEAN).most_common(5)
     print(COUNT)
 
     # print(extract_links("http://news.ycombinator.com", headers=HEADER))
