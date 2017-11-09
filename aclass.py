cfb0fd8602cd14b433da0e1a0eeaf4680e205ddc
alvivar <andresalvivar@gmail.com>
2017-11-09 07:02:31 -0600

    Url classifier prototype, currently html cleaning and word count from an url

---
+++ aclass.py
@@ -0,0 +1,77 @@
+import json
+import os
+import re
+import sys
+from collections import Counter
+
+import requests
+from bs4 import BeautifulSoup
+
+from lxml import html
+from lxml.html.clean import clean_html
+
+
+def extract_links(url, *, headers={}):
+    page = requests.get(url, headers=HEADER)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    return [
+        a['href'] for a in soup.find_all("a") if a['href'].startswith("http")
+    ]
+
+
+def extract_words(url, *, headers={}):
+    page = requests.get(url, headers=headers)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    tree = html.fromstring(soup.get_text())
+    clean_tree = clean_html(tree)
+    text = clean_tree.text_content()
+
+    with open("dump.txt", "w", encoding="utf-8") as f:
+        f.write(text)
+
+    return re.split("[^a-zA-Z0-9]", text)
+
+
+def extract_words2(url, *, headers={}):
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.content, "lxml")
+
+    for script in soup(["script", "style"]):
+        script.decompose()
+    text = soup.get_text().split()
+
+    with open("dump.txt", "w", encoding="utf-8") as f:
+        f.write("\n".join(text))
+
+    return text
+
+
+if __name__ == "__main__":
+
+    # frozen? (cxfreeze compatibility)
+    DIR = os.path.normpath(
+        os.path.dirname(
+            sys.executable if getattr(sys, 'frozen', False) else __file__))
+    os.chdir(DIR)
+
+    HEADER = {
+        'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+    }
+
+    STOP_ES = json.load(open("stop-es.json", "r"))
+    STOP_EN = json.load(open("stop-en.json", "r"))
+    STOP_WORDS = STOP_EN + STOP_ES
+
+    WORDS = extract_words2(
+        "https://stackoverflow.com/questions/8113782/split-string-on-whitespace-in-python",
+        headers=HEADER)
+    CLEAN = [w for w in WORDS if w and w.lower() not in STOP_WORDS]
+
+    COUNT = Counter(CLEAN)
+    print(COUNT)
+
+    # print(extract_links("http://news.ycombinator.com", headers=HEADER))
