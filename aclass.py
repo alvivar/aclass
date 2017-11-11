46bb38249ad96a8d5cd5249f5d52e0f46b6c773c
alvivar <andresalvivar@gmail.com>
2017-11-10 20:04:46 -0600

    Better split | Able to analyze list of files

--- aclass.py
+++ aclass.py
@@ -9,7 +9,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def extract_links(html_text):
+def extract_urls(html_text):
     soup = BeautifulSoup(html_text, "html.parser")
     return [
         a['href'] for a in soup.find_all("a") if a['href'].startswith("http")
@@ -22,10 +22,10 @@ def extract_words(html_text, *, ignore=[]):
     for script in soup(["script", "style"]):
         script.decompose()
 
-    words = soup.get_text().split()
-    clean_words = [i for i in words if i and i.lower() not in ignore]
+    dirty = re.split('[^a-zA-Z]', soup.get_text().lower())
+    clean = [i for i in dirty if i and i not in ignore]
 
-    return clean_words
+    return clean
 
 
 def categorize_top_words(top_words):
@@ -59,10 +59,19 @@ if __name__ == "__main__":
         const="bookmark.html")
     ARGS = PARSER.parse_args()
 
-    # Urls are needed, of course
+    # Urls from files to the urls list | -f
+    FILE_URLS = []
+    for f in ARGS.files:
+        with open(f, "r", encoding="utf-8") as f:
+            text = f.read()
+            urls = extract_urls(text)
+            FILE_URLS += urls
+
+    # Urls are needed, of course, including from the files
+    ARGS.urls += FILE_URLS
     if not ARGS.urls:
         PARSER.print_usage()
-        print("urls are needed!")
+        print("urls are needed, try -u or -f")
         PARSER.exit()
 
     # To ignore
@@ -70,19 +79,28 @@ if __name__ == "__main__":
     STOP_EN = json.load(open("stop-en.json", "r"))
     STOP_WORDS = STOP_EN + STOP_ES
 
-    # Extraction
+    # Extraction and word count | -u
     HEADERS = {
         'User-Agent':
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
     }
 
-    TOP_WORDS = []
+    TOP = []
     for url in ARGS.urls:
-        html = requests.get(url, headers=HEADERS)
-        words = extract_words(html.content, ignore=STOP_WORDS)
-        count = Counter(words).most_common(10)
-        TOP_WORDS.append((url, count))
+        print(f"Analyzing {url}")
 
-    print(TOP_WORDS)
+        try:
+            html = requests.get(url, headers=HEADERS)
+            words = extract_words(html.content, ignore=STOP_WORDS)
+            count = Counter(words).most_common(10)
+        except requests.exceptions.ConnectionError:
+            continue
 
-    # print(extract_links("http://news.ycombinator.com", headers=HEADER))
+        TOP.append((url, count))
+
+    # Sexy print
+    for url in TOP:
+        print(f"\n{url[0]}")
+        for count in url[1]:
+            print(f"({count[0]} {count[1]}) ", end='')
+        print()
